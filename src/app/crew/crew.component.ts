52f3c3ad3f3794072a15585973990038440e6582
Angela Mitchell <anglarenmit@gmail.com>
2022-03-08 22:49:57 -0600

    initial commit

---
+++ src/app/crew/crew.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css'],
+})
+export class CrewComponent implements OnInit {
+  inCrew: boolean = false;
+  crew: object[] = [];
+
+  candidates: object[] = [
+    {
+      name: 'Sally Ride',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/sally-ride.jpg',
+    },
+    {
+      name: 'Mae Jemison',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg',
+    },
+    {
+      name: 'Ellen Ochoa',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg',
+    },
+    {
+      name: 'Frederick Gregory',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg',
+    },
+    {
+      name: 'Guion Bluford',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg',
+    },
+    {
+      name: 'Kjell Lindgren',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg',
+    },
+    {
+      name: 'Jeanette Epps',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addCrewMember(candidate: object) {
+    if (!this.crew.includes(candidate) && this.crew.length < 3) {
+      this.crew.push(candidate);
+    } else if (this.crew.includes(candidate)) {
+      let index = this.crew.indexOf(candidate);
+      this.crew.splice(index, 1);
+    }
+  }
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  missionName: string = 'LC Angular 3';
+  rocketName: string = 'ICAN';
+  editingRocket: boolean = false;
+  editingMission: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  updateMission(updatedName: string) {
+    this.missionName = updatedName;
+    this.editingMission = false;
+  }
+
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
+  }
+}
